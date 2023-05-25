e52ac068b72f73f8a99b49d112f81d7c7ce3a4be
Mihail Rkitcky <papi.pay@mail.ru>
2023-05-25 20:38:36 +0300

    Задача с VendingMachine

---
+++ OOP_HOMEWORK1/VendingMachines.java
@@ -0,0 +1,8 @@
+package OOP_HOMEWORK1;
+
+public interface VendingMachines {
+    
+    abstract void acceptingMoney();
+    abstract void productDelivery();
+    abstract void deliveryOfChange();
+}
